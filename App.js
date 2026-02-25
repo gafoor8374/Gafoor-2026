ab759b05c93b122e859f590cd7971e153badedd3
Shaik Gafoor <shaikgafoor@MacBookAir.lan>
2026-02-25 12:04:09 +0530

    Episode-03

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1", key: "child1" }, [
-    React.createElement("h1", {id:"h1 child1", key:"h1-child1"}, "I am h1 tag"),
-    React.createElement("h2", {id:"h2 child1", key:"h2-child1"}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2", key: "child2" }, [
-    React.createElement("h1", {id:"h1 child2", key:"h1-child2"}, "I am h1 tag"),
-    React.createElement("h2", {id:"h2 child2", key:"h2-child2"}, "I am h2 tag"),
-  ]),
-]);
+const Title = () =>(
+    <h1>It is Heading title</h1>
+)
+const HeadingComponent = () => (
+    <div id="container">
+            <Title> </Title>
+            <h1 className="heading">
+                Welcome to the React Course
+
+            </h1>
+        </div>
+)
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
